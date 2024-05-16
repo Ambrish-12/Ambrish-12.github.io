40f201c920130886f5c2f52f3d365723e3e93565
Ambrish-12 <aambrishambrish@gmail.com>
2024-05-16 08:57:30 +0530

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Ambrish-12.github.io',
+  base:'/Ambrish-12.github.io/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
